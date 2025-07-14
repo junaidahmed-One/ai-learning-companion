fbc918f254623dd2440b028bb4bd75795d1434e8
junaidahmed-One <junaidahmed882000@gmail.com>
2025-07-14 17:12:14 +0530

    git fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import {Button} from "@/components/ui/button";
 const Page = () => {
   return (
     <div>
-      <h1 className="text-2xl underline">Welcome to my Saas</h1>
+      <h1 className="text-2xl underline">Welcome to my Saas app</h1>
         <Button>Let&#39;s get started</Button>
     </div>
   )
