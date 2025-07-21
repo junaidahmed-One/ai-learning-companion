a2da92b1b379a93bb9388927e9c3f8e1d57e2baf
junaidahmed-One <junaidahmed882000@gmail.com>
2025-07-21 14:59:01 +0530

    Implement initial Companion Form

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import CompanionForm from "@/components/CompanionForm";
 
 
 const NewCompanion = () => {
     return (
-        <div>
-            <h1 className="text-2xl underline">NewCompanion</h1>
-
-        </div>
+        <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
+            <article className="w-full gap-4 flex flex-col">
+                <h1>
+                    Companion Builder
+                </h1>
+                <CompanionForm />
+            </article>
+        </main>
     )
 }
 
