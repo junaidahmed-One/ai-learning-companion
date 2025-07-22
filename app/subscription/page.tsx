1e72ff0fcb6bd414e1ccec14e14fdd8545a1b672
junaidahmed-One <junaidahmed882000@gmail.com>
2025-07-22 12:36:53 +0530

    Implement clerk billing plans

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import {PricingTable} from "@clerk/nextjs";
 
 
 const Subscription = () => {
     return (
         <div>
-            <h1 className="text-2xl underline">Subscription</h1>
+            <h1 className="text-2xl underline">
+                <PricingTable/>
+            </h1>
         </div>
     )
 }
