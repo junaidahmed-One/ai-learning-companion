78cc5026cca3d52d610331972c89ae901aa67190
junaidahmed-One <junaidahmed882000@gmail.com>
2025-08-02 23:22:20 +0530

    Implement session history

--- app/my-profile/page.tsx
+++ app/my-profile/page.tsx
@@ -1,10 +1,36 @@
-import React from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import {
+  getUserCompanions,
+  getUserSessions,
+} from "@/lib/actions/companion.actions";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const Profile = () => {
+const Profile = async () => {
+  const user = await currentUser();
+
+  if (!user) redirect("/sign-in");
+
+  const companions = await getUserCompanions(user.id);
+  const sessionHistory = await getUserSessions(user.id);
+
   return (
-    <div>
-      <h1 className="text-2xl underline">Profile</h1>
-    </div>
+    <main className="min-lg:w-3/4">
+      <section></section>
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </main>
   );
 };
 
